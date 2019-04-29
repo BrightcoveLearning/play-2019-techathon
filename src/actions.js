1a0da8f8a9d3017eaf28bdf21b841445809c6ccb
ldayananda <ldayananda@brightcove.com>
2019-04-29 15:33:21 -0400

    initial cms actions

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,15 @@
 /** see https://redux-docs.netlify.com/basics/actions */
 
 /** Action Types */
-export const PLACEHOLDER_ACTION = 'PLACEHOLDER_ACTION';
+export const LIST_VIDEOS_BY_ACCOUNT = 'LIST_VIDEOS_BY_ACCOUNT';
+
 
 /** Constants */
 
 /** Action Creators */
+export default function listVideosByAccountAction(videos) {
+  return {
+    type: LIST_VIDEOS_BY_ACCOUNT,
+    videos
+  };
+};
--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,20 @@
 import * as actions from './actions';
 
-const initialState = {};
+const initialState = {
+  accountId: 6027103981001,
+  videos: []
+};
 
 /** see https://redux-docs.netlify.com/basics/reducers */
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case actions.LIST_VIDEOS_BY_ACCOUNT:
+      return Object.assign({}, state, {
+        videos: action.videos
+      });
+    default:
+      return state;
+  }
 };
 
 export {
