ddd7dbf76529f54cb0bb913e4969fbd795b87ec6
ldayananda <ldayananda@brightcove.com>
2019-05-01 11:54:49 -0400

    make api calling helper

--- src/actions.js
+++ src/actions.js
@@ -26,6 +26,28 @@ export function receiveVideoList(accountId = '6027103981001', data) {
   };
 };
 
+function makeApiCall(url) {
+  return fetch(oAuthProxyUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      'apiCall': url,
+      'method': 'GET'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(
+    (response) => response.json(),
+
+    // Do not use catch, because that will also catch
+    // any errors in the dispatch and resulting render,
+    // causing a loop of 'Unexpected batch number' errors.
+    // https://github.com/facebook/react/issues/6895
+    (error) => console.error('ERROR', error)
+  );
+}
+
 /**
  * see https://redux-docs.netlify.com/advanced/async-actions#async-action-creators
  */
@@ -34,27 +56,11 @@ export function fetchVideoList(accountId) {
     // request video list
     dispatch(requestVideoList(accountId));
 
-    return fetch(oAuthProxyUrl, {
-      method: 'POST',
-      body: JSON.stringify({
-        'apiCall': `https://cms.api.brightcove.com/v1/accounts/6027103981001/videos`,
-        'method': 'GET'
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(
-      (response) => response.json(),
-
-      // Do not use catch, because that will also catch
-      // any errors in the dispatch and resulting render,
-      // causing a loop of 'Unexpected batch number' errors.
-      // https://github.com/facebook/react/issues/6895
-      (error) => console.error('ERROR', error)
+    return makeApiCall(
+      `https://cms.api.brightcove.com/v1/accounts/6027103981001/videos`
     )
     .then((json) => {
-      dispatch(receiveVideoList(accountId, json))
+      return dispatch(receiveVideoList(accountId, json))
     });
   }
 };
