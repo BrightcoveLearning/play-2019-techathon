2ff36731f85ce480a8d19c792176571651fe7dd3
Kevin Reynolds <RevinKey@users.noreply.github.com>
2019-05-03 10:54:38 -0400

    Bc player (#4)
    * Brightcove Player implemented
    
    * Brightcove Player implemented
    
    * fix package.json

--- src/reducers.js
+++ src/reducers.js
@@ -2,35 +2,35 @@
 import * as actions from './actions';
 
 const initialState = {
-  accountId: 6027103981001,
-  videos: [],
-  selectedVideo: null,
-  analyticData: null
+    accountId: 6027103981001,
+    videos: [],
+    selectedVideo: null,
+    analyticData: null
 };
 
 const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actions.REQUEST_VIDEO_LIST:
-      return Object.assign({}, state, {
-        accountId: action.accountId
-      });
-    case actions.RECEIVE_VIDEO_LIST:
-      return Object.assign({}, state, {
-        videos: action.videos
-      });
-    case actions.RECEIVE_SELECTED_VIDEO:
-      return Object.assign({}, state, {
-        selectedVideo: action.selectedVideo
-      });
-    case actions.RECEIVE_VIDEO_ANALYTICS:
-      return Object.assign({}, state, {
-        analyticData: action.analyticData
-      });
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case actions.REQUEST_VIDEO_LIST:
+            return Object.assign({}, state, {
+                accountId: action.accountId
+            });
+        case actions.RECEIVE_VIDEO_LIST:
+            return Object.assign({}, state, {
+                videos: action.videos
+            });
+        case actions.RECEIVE_SELECTED_VIDEO:
+            return Object.assign({}, state, {
+                selectedVideo: action.selectedVideo
+            });
+        case actions.RECEIVE_VIDEO_ANALYTICS:
+            return Object.assign({}, state, {
+                analyticData: action.analyticData
+            });
+        default:
+            return state;
+    }
 };
 
 export {
-  rootReducer
+    rootReducer
 };
