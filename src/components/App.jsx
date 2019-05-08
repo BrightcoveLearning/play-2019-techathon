de49fe891bb3daa3759a0f6f7ce769a0a8c2ec19
ldayananda <ldayananda@users.noreply.github.com>
2019-05-08 18:14:34 -0400

    cleanup base project (#22)
    * update package-lock.json
    
    * clean up tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import VideoIdDropdown from './VideoIdDropdown';
-import AnalyticsFetcher from './AnalyticsFetcher';
-import BrightcovePlayer from './BrightcovePlayer';
-import VideoUploader from './VideoUploader';
 
 export default class App extends Component {
-  constructor (props) {
-    super(props);
-    this.handleVideoChange = this.handleVideoChange.bind(this);
-    this.state = {
-      selectedVideo: null
-    };
-  }
-
-  handleVideoChange (video) {
-    this.setState({ selectedVideo: video });
-  };
   render () {
     return (
       <div className='App'>
-        <VideoIdDropdown onHandleVideoChange={this.handleVideoChange} />
-        <BrightcovePlayer selectedVideo={this.state.selectedVideo} />
-        <AnalyticsFetcher selectedVideo={this.state.selectedVideo} />
-        <VideoUploader />
       </div>
     );
   }
